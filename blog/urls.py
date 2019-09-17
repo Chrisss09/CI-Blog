e87159dc213a6f4d9465d980b0b9f78ea5e0d27a
Chris Fowler <chris.fowler73@hotmail.co.uk>
2019-09-17 18:33:42 +0100

    Fixed image path

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
-from django.views.static import serve
-from .settings import MEDIA_ROOT
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', RedirectView.as_view(url='posts/')),
     path('posts/', include('posts.urls')),
-    path('media/<int:path_id>/', serve, {'document_root': MEDIA_ROOT}),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
